bf1ea17b4e7fb9b6a8a21d767ef7920cab570606
Ali Raza <alirazakhalid1129@gmail.com>
2025-08-24 00:04:30 +0500

    feat add db/prisma

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,18 @@
-import { Button } from "@workspace/ui/components/button"
+import { Button } from "@workspace/ui/components/button";
+import { prisma } from "@repo/db";
 
-export default function Page() {
+export default async function Page() {
+  const user = await prisma.user.findFirst();
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World</h1>
+        {user ? (
+          <h1 className="text-2xl font-bold">{user?.name}</h1>
+        ) : (
+          <h1>No User added.</h1>
+        )}
         <Button size="sm">Button</Button>
       </div>
     </div>
-  )
+  );
 }
---
+++ packages/db/README.md
@@ -0,0 +1,10 @@
+# `db`
+
+```json
+"scripts": {
+    "db:generate": "prisma generate",
+    "db:migrate": "prisma migrate dev --skip-generate",
+    "db:deploy": "echo 'Add deploy script here'",
+    "db:studio": "prisma studio"
+  },
+```
